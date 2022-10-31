c238279980f0ffe772b3f283c7589eaa6a7f261d
Albana Ahmeti <bana.ahmeti@hotmail.com>
2022-10-31 13:38:50 +0100

    shop page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* menu */
+/* menu for small devices mq */
 const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
@@ -13,3 +13,19 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+/*Single Product slider */
+var MainImg = document.getElementById("MainImg");
+var smallimg = document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function () {
+  MainImg.src = smallimg[0].src;
+};
+smallimg[1].onclick = function () {
+  MainImg.src = smallimg[1].src;
+};
+smallimg[2].onclick = function () {
+  MainImg.src = smallimg[2].src;
+};
+smallimg[3].onclick = function () {
+  MainImg.src = smallimg[3].src;
+};
